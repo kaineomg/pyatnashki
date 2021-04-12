90fad5163f0227fcf4016f3cafd6201d9766de6f
kaineomg <mgt62rus@gmail.com>
2021-04-12 21:26:11 +0300

    first

---
+++ 15.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <time.h>
+#include<stdlib.h>
+using namespace std;
+
+
+int main()
+{
+
+	int const size = 16;
+	int numbers[size] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0};	
+		for (int i = 0;i < size;i++)
+		{
+			cout << numbers[i] << "\t";
+			if (i == 3 || i == 7 || i == 11)
+			{
+				cout << "\n\n\n";
+				
+			}
+		}
+		
+	
+		int temp;
+		srand(time(0));
+		for (int i = 0; i < size; i++) 
+		{
+			int random = rand() % size;
+			temp = numbers[i];
+			numbers[i] = numbers[random];
+			numbers[random] = temp;
+
+		}
+		cout << endl<< endl << endl << endl;
+		
+		void vivod (int numbers[], int size);
+		{
+
+			for (int i = 0;i < size;i++)
+			{
+				cout << numbers[i] << "\t";
+				if (i == 3 || i == 7 || i == 11)
+				{
+					cout << "\n\n\n";
+
+				}
+			}
+		}
+
+}
+
+
