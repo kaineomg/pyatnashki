21f8d7f90407719da60ab5a6d822d3b407134620
kaineomg <mgt62rus@gmail.com>
2021-04-19 20:24:00 +0300

    2

--- 15.cpp
+++ 15.cpp
@@ -4,35 +4,41 @@
 using namespace std;
 
 
+
+void mix(int numbers[], int size);
+
+void vivod(int numbers[], int size);
+
 int main()
 {
-
 	int const size = 16;
-	int numbers[size] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0};	
-		for (int i = 0;i < size;i++)
-		{
-			cout << numbers[i] << "\t";
-			if (i == 3 || i == 7 || i == 11)
-			{
-				cout << "\n\n\n";
-				
-			}
-		}
-		
+	int numbers[size] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0 };
+	vivod(numbers, size);
+	mix(numbers, size);
+	vivod(numbers, size);
 	
-		int temp;
-		srand(time(0));
-		for (int i = 0; i < size; i++) 
-		{
-			int random = rand() % size;
-			temp = numbers[i];
-			numbers[i] = numbers[random];
-			numbers[random] = temp;
 
+}
+
+
+	
+void mix(int numbers[], int size)
+		{
+			int temp;
+			srand(time(0));
+			for (int i = 0; i < size; i++)
+			{
+				int random = rand() % size;
+				temp = numbers[i];
+				numbers[i] = numbers[random];
+				numbers[random] = temp;
+
+			}
+
+			cout << endl << endl << endl << endl;
 		}
-		cout << endl<< endl << endl << endl;
-		
-		void vivod (int numbers[], int size);
+void vivod (int numbers[], int size)
+
 		{
 
 			for (int i = 0;i < size;i++)
@@ -44,8 +50,11 @@ int main()
 
 				}
 			}
+			cout << endl << endl << endl << endl;
 		}
+		
+		
+		
 
-}
 
 
