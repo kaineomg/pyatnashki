da892f06a60abee1d7e57017c37b3a7e5234f10b
kaineomg <mgt62rus@gmail.com>
2021-04-21 20:53:11 +0300

    commit3

--- 15.cpp
+++ 15.cpp
@@ -52,6 +52,76 @@ void vivod (int numbers[], int size)
 			}
 			cout << endl << endl << endl << endl;
 		}
+
+void example(int number[])
+{
+	for (int i = 0; i < 16; i++)
+	{
+		if (i != 15)
+			number[i] = i + 1;
+		else
+			number[i] = 0;
+	}
+}
+
+void move(int numbers[])
+{
+	int key, temp = 0;
+	
+
+	while (true)
+	{
+		vivod(numbers, 16);
+		symbols();
+		temp = findZero(numbers);
+
+		key = getch();
+		if (key == 224)
+			key = getch();
+
+		if (key == 72 && temp < 12) // Up
+		{
+			numbers[temp] = numbers[temp + 4];
+			numbers[temp + 4] = 0;
+		}
+		else if (key == 80 && temp > 3) // Down
+		{
+			numbers[temp] = numbers[temp - 4];
+			numbers[temp - 4] = 0;
+		}
+		else if (key == 75 && temp != 3 && temp != 7 && temp != 11 && temp != 15) // Left
+		{
+			numbers[temp] = numbers[temp + 1];
+			numbers[temp + 1] = 0;
+		}
+		else if (key == 77 && temp % 4 != 0) // Right
+		{
+			numbers[temp] = numbers[temp - 1];
+			numbers[temp - 1] = 0;
+		}
+		else if (key == 96 && cheat == true) // ` 
+		{
+			
+		}
+		else if (key == 23) // ctrl + W
+		{
+			example(numbers);
+			vivod(numbers, 16);
+			symbols();
+		}
+		else if (key == 27) // esc
+		{
+			break;
+		}
+
+		if (cheker(arr, 16))
+		{
+			show(arr, 16);
+			symbols();
+			break;
+		}
+	}
+}
 		
 		
 		
